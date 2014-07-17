b288cac41658e8ea2892003af18a3deed14fe32e
otaQ <otaq.hsc@gmail.com>
2014-07-17 21:24:38 +0800

    validates allow float

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ActiveRecord::Base
   validates :name, presence: true
-  validates :price, :quantity, numericality: { only_integer: true }
+  validates :price, :quantity, numericality: { only_integer: false }
 end
