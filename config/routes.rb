d93ffcac76a4a2ceb566626015b834fe129fd9b1
Tony Jian <tonytonyjan@gmail.com>
2014-07-21 18:53:43 +0800

    fix routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'pages#index'
-  get :about, :index, :contact, controller: :pages
+  get :about, :contact, controller: :pages
   resources :products
 end
