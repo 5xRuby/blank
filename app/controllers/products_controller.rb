2359bf01d58f0d47d3f2f0637a158c5448225566
otaQ <otaq.hsc@gmail.com>
2014-07-28 01:22:50 +0800

    homework

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_product, only: [:show, :edit, :update, :destroy]
+  before_action :get_categories, only: [:new, :edit]
 
   # GET /products
   # GET /products.json
@@ -78,8 +79,12 @@ class ProductsController < ApplicationController
       @product = Product.find(params[:id])
     end
 
+    def get_categories
+      @categories = Category.select([:name, :id])
+    end
+
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :name_confirmation, :description, :price, :volume)
+      params.require(:product).permit(:name, :name_confirmation, :description, :price, :volume, :category_id)
     end
 end
