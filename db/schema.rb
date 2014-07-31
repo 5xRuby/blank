ea7eaae15f5b084d3057ddbf4f2e3a3b2a2ef677
Tony Jian <tonytonyjan@gmail.com>
2014-07-31 22:47:00 +0800

    add cart model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140727124719) do
+ActiveRecord::Schema.define(version: 20140731142452) do
+
+  create_table "carts", force: true do |t|
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "carts", ["user_id"], name: "index_carts_on_user_id"
 
   create_table "categories", force: true do |t|
     t.string   "name"
