22132ef969faa549cbc0d4c5405a1d3d9626151d
Tony Jian <tonytonyjan@gmail.com>
2014-08-01 16:37:34 +0800

    show price

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,8 @@
 class Cart < ActiveRecord::Base
   belongs_to :user
   has_many :line_items
+
+  def total
+    line_items.to_a.sum(&:subtotal)
+  end
 end
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,4 +1,8 @@
 class LineItem < ActiveRecord::Base
   belongs_to :cart
   belongs_to :product
+
+  def subtotal
+    product.price * volume
+  end
 end
