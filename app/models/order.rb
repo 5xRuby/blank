103d58c6de50afd1ef14d02908b58f9f24b097b6
Tony Jian <tonytonyjan@gmail.com>
2014-08-05 17:48:42 +0800

    create ordre from cart

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,22 @@
 class Order < ActiveRecord::Base
   belongs_to :user
   has_many :order_items
+
+  def self.new_for cart
+    order = Order.new user: cart.user
+    cart.line_items.includes(:product).each do |line_item|
+      order_item = OrderItem.new({
+        product_name: line_item.product.name,
+        product_price: line_item.product.price,
+        volume: line_item.volume
+      }) 
+      order.order_items << order_item
+    end
+    order
+  end
+
+  def self.create_for cart
+    order = new_for cart
+    order.create
+  end
 end
